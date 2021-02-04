5cba71ed8aef19943704bab82070ea19b13d0b1e
Mohammad Rakibul Hasan <rkhasan333@gmail.com>
2021-02-04 21:49:42 +0600

    tide up

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -19,10 +19,8 @@ const Pagination = (props) => {
             <ul className="pagination">
                 {pages.map (page => 
                      <li key={page} className={ page === currentPage  ? 'page-item active' : 'page-item'}>
-                         <a className= "page-link" onClick = {() => onPageChange(page)}>
-                              {page} 
-                            </a>
-                         </li>
+                         <a className= "page-link" onClick = {() => onPageChange(page)}> {page} </a>
+                    </li>
                      )}
             </ul>
         </nav>
