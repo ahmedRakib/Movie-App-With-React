282e28f02546b6689081f609037359b01ed64b0b
Mohammad Rakibul Hasan <rkhasan333@gmail.com>
2021-06-02 22:25:59 +0200

    code to save and edit  movie

---
+++ src/components/common/select.jsx
@@ -0,0 +1,21 @@
+const Select = (props) => {
+    const {name, label, options, error, value, onChange} = props
+
+    return ( 
+    <div className="form-group">
+        <label htmlFor={ name }>{ label }</label>
+        <select name = {name} id = {name} value = {value} onChange ={onChange}  className="form-control">
+            <option value=""/>
+            {options.map(option => (
+                <option key={option._id} value={option._id}>
+                    {option.name}
+                </option>
+            ))}
+        </select> 
+        {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+   
+  );
+};
+ 
+export default Select;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -72,6 +72,8 @@ class Movies extends Component {
           />
         </div>
         <div className="movieTable">
+          <Link to="/movies/new" className="btn btn-primary">New Movie</Link>
+          <br></br>
           <p>Showing {filteredMovies.length} in the database</p>
 
           {this.movies}
